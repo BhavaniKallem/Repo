53c54e667785f30d9f66d138581e5b16470104be
BhavaniKallem <60298564+BhavaniKallem@users.noreply.github.com>
2020-02-05 23:07:25 +0530

    Delete File1.java

--- File1.java
+++
@@ -1,6 +0,0 @@
-import java.util.*;
-public class File1{
-    public static void main(String args[]){
-        System.out.println("HI THIS IS MY FILE ADDED TO GIT REPOSITORY USING COMMAND LNE");
-    }
-}
